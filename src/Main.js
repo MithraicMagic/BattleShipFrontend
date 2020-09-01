bfa9ae4d7daf43fdc2dcfd684fa2156460ead980
Nick Walraven <nickelbink@gmail.com>
2020-09-01 19:30:31 +0200

    - Socket setup

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
+import io from 'socket.io-client';
+
+const socket = io('http://bs.cesarpulles.nl/sockets');
 
 export default class Main extends Component {
+    componentDidMount() {
+        socket.emit('chatevent', {username: 'henk', message: 'henk'});
+        socket.on('chatevent', (data) => console.log(data));
+    }
+    
     render() {
         return (
             <div>
