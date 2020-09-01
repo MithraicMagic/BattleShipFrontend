132d3fd4d5ad140061eb3cfab9cccfbb39cd29aa
MithraicMagic <nickelbink@gmail.com>
2020-09-01 21:14:43 +0200

    Ultra push!!!

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import io from 'socket.io-client';
 
-const socket = io('https://bs.cesarpulles.nl/sockets');
+import './main.scss';
+
+const socket = io('https://bs.cesarpulles.nl', {path: '/sockets'});
+// const socket = io('http://localhost:6003', {path: '/sockets'});
 
 export default class Main extends Component {
     componentDidMount() {
         socket.on('connect', () => console.log('suck'));
+        socket.on('error', (error) => console.error(error));
         socket.emit('chatevent', {username: 'henk', message: 'henk'});
         socket.on('chatevent', (data) => console.log(data));
     }
