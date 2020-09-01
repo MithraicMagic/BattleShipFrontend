d76fe33b0f92a862fd1fc110890b21205f562aaa
MithraicMagic <nickelbink@gmail.com>
2020-09-01 21:33:35 +0200

    Ultimate fix

--- src/Main.js
+++ src/Main.js
@@ -3,13 +3,10 @@ import io from 'socket.io-client';
 
 import './main.scss';
 
-const socket = io('https://bs.cesarpulles.nl', {path: '/sockets'});
-// const socket = io('http://localhost:6003', {path: '/sockets'});
+const socket = io(process.env.REACT_APP_URL, {path: '/sockets'});
 
 export default class Main extends Component {
     componentDidMount() {
-        socket.on('connect', () => console.log('suck'));
-        socket.on('error', (error) => console.error(error));
         socket.emit('chatevent', {username: 'henk', message: 'henk'});
         socket.on('chatevent', (data) => console.log(data));
     }
