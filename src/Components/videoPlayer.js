81ba8664ab5b0387d1ca9809aba9676d77a667a0
MithraicMagic <nickelbink@gmail.com>
2020-09-30 21:39:27 +0200

    - De ultimate youtube player, is nu nog ultimater!

--- src/Components/videoPlayer.js
+++ src/Components/videoPlayer.js
@@ -9,16 +9,22 @@ export default class videoPlayer extends Component {
 
     play(link) {
         this.setState({
-            video: <iframe title="YouTube Player" width="1280" height="720" src={link} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+            video: <iframe title="YouTube Player" src={link} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
         });
         return true;
     }
 
     render() {
-        return (
-            <div className="youtube-player">
-                {this.state.video}
-            </div>
-        )
+        if (this.state.video) {
+            return (
+                <div id="player" className="youtube-player closed" onClick={(e) => e.target === document.getElementById('close-button') ? null : document.getElementById('player').classList.remove('closed')}>
+                    <h1>Open Youtube Player</h1>
+                    <button id="close-button" onClick={() => document.getElementById('player').classList.add('closed')}>Close</button>
+                    {this.state.video}
+                </div>
+            )
+        }
+
+        return null;
     }
 }
